6a77f312b60d9ecda4c10185abd8ad459b91fb3a
michalporz <accidentalpicasso@gmail.com>
2014-11-19 13:40:28 +0100

    test fix

--- test/repo_test.js
+++ test/repo_test.js
@@ -8,75 +8,85 @@ var exec = require('child-process-promise').exec;
 var should = require("should");
 var fs = require("fs");
 
-exec("rm -r ./repos")
-  .then(function() {
-    fs.unlink("database.sqlite", function() {
-      db.createTables('./database.sqlite', function() {
+
+
+
+    describe('add_new_project', function () {
+
+      before(function(done) {
+        this.timeout(10000);
+        exec("rm -r ./repos")
+          .then(function() {
+            done();
+          }).fail( function() {
+            done();
+          });
       });
-    });
-  });
+      before(function(done) {
+        fs.unlink("database.sqlite", function () {
+          db.createTables('./database.sqlite', function () {
+            done();
+          });
+        });
+      });
 
 
+      var project1 = {
+        "projectName": "svn_test_mocha",
+        "repositoryUrl": "https://subversion.assembla.com/svn/mp-svntest/",
+        "repositoryType": "svn",
+        "cronePattern": "* * * * *",
+        "scriptPath": "echo.sh"
+      };
 
-describe('add_new_project', function() {
+      project2 = {
+        "projectName": "git_test_mocha",
+        "repositoryUrl": "git://github.com/test1git/test",
+        "repositoryType": "git",
+        "cronePattern": "* * * * *",
+        "scriptPath": "echo.sh"
+      };
 
-  var project1 = {
-    "projectName": "svn_test_mocha",
-    "repositoryUrl": "https://subversion.assembla.com/svn/mp-svntest/",
-    "repositoryType": "svn",
-    "cronePattern": "* * * * *",
-    "scriptPath": "echo.sh"
-  };
+      before(function (done) {
 
-  project2 = {
-    "projectName": "git_test_mocha",
-    "repositoryUrl": "git://github.com/test1git/test",
-    "repositoryType": "git",
-    "cronePattern": "* * * * *",
-    "scriptPath": "echo.sh"
-  };
-
-  before(function(done) {
-
-    svn.checkout(db, project1, './repos/');
-    git.gitClone(db,project2,'./repos');
-    done();
-
-  });
-
-
-  it('should add new SVN project to database and create dir', function(done) {
-
-    this.timeout(10000);
-
-    var dbProject = db.findInstance('Project', {where: {project_name: project1.projectName}});
-    dbProject.then(function (projects) {
-      projects.should.not.equal(0);
-
-      fs.exists("repos/" + project1.projectName, function (exists) {
-        exists.should.be.ok;
+        svn.checkout(db, project1, './repos/');
+        git.gitClone(db, project2, './repos');
         done();
+
+      });
+
+
+      it('should add new SVN project to database and create dir', function (done) {
+
+        this.timeout(10000);
+
+        var dbProject = db.findInstance('Project', {where: {project_name: project1.projectName}});
+        dbProject.then(function (projects) {
+          projects.should.not.equal(0);
+
+          fs.exists("repos/" + project1.projectName, function (exists) {
+            exists.should.be.ok;
+            done();
+          });
+
+        });
+      });
+
+
+      it('should add new GIT project to database and create dir', function (done) {
+
+        this.timeout(10000);
+
+        var dbProject = db.findInstance('Project', {where: {project_name: project2.projectName}});
+        dbProject.then(function (projects) {
+          projects.should.not.equal(0);
+
+          fs.exists("repos/" + project2.projectName, function (exists) {
+            exists.should.be.ok;
+            done();
+          });
+
+        });
       });
 
     });
-  });
-
-
-
-  it('should add new GIT project to database and create dir', function(done) {
-
-    this.timeout(10000);
-
-    var dbProject = db.findInstance('Project', {where: {project_name: project2.projectName}});
-    dbProject.then(function (projects) {
-      projects.should.not.equal(0);
-
-      fs.exists("repos/" + project2.projectName, function (exists) {
-        exists.should.be.ok;
-        done();
-      });
-
-    });
-  });
-
-});
