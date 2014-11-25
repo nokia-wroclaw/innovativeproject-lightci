f10d40a96859508ef78cb5d85f5fb92ef39c26be
Mateusz Sikora <ms1qaz@gmail.com>
2014-11-26 00:54:01 +0100

    added trends

--- client/components/projectsTable/projectsTable.controller.js
+++ client/components/projectsTable/projectsTable.controller.js
@@ -9,19 +9,46 @@ angular.module('lightciApp')
 
     $scope.baseUrl = '#';
 
-    $http.get('/api/projects').success(function(proj) {
+    $http.get('/api/projects').success(function (proj) {
+      proj.forEach(function (project) {
+        var last = project.lastBuilds[0];
+        var quantityTrue = 0;
+        var quantityFalse = 0;
+        project.lastBuilds.forEach(function (lastBuild) {
+          if (lastBuild) {
+            quantityTrue++;
+          } else {
+            quantityFalse++;
+          }
+        });
+        if (quantityTrue == 1 && quantityFalse > 0 && last == true)
+          project.trend = 'love';
+        else if (quantityFalse == 1 && quantityTrue > 0 && last == false)
+          project.trend = 'suprise';
+        else if (quantityTrue > 0 && quantityFalse == 0)
+          project.trend = 'verryhappy';
+        else if (quantityTrue > quantityFalse && quantityFalse > 0)
+          project.trend = 'happy';
+        else if (quantityTrue < quantityFalse && quantityFalse > 0)
+          project.trend = 'sad';
+        else if (quantityTrue == 0 && quantityFalse > 0)
+          project.trend = 'verrysad';
+        else
+          project.trend = 'else';
+
+      });
       $scope.projects = proj;
     });
 
-    $scope.buildProject = function(id) {
-      var data = { project_id: id };
+    $scope.buildProject = function (id) {
+      var data = {project_id: id};
       $http.post('/api/buildnow', data).success(function () {
 
       });
-    }
+    };
 
-    $scope.removeProject = function(id) {
-      var data = { project_id: id };
+    $scope.removeProject = function (id) {
+      var data = {project_id: id};
       $http.post('/api/remove', data).success(function () {
 
       });
--- server/api/project/project.controller.js
+++ server/api/project/project.controller.js
@@ -2,12 +2,29 @@
 
 var _ = require('lodash');
 var db = require('../../components/db/db');
-
+var Q = require("q");
 // Get list of projects
-exports.index = function(req, res) {
-  db.findInstance('Project',{})
-    .then(function(projects){
-      res.json(projects);
+exports.index = function (req, res) {
+  var projects = [];
+  db.findInstance('Project', {}).then(function (dbProjects) {
+    projects = _.map(dbProjects,function(dbProject){
+      return dbProject.dataValues;
     });
+    var promises = _.map(projects, function (project) {
+      return db.findInstance('Build', {where: {ProjectId: project.id}, limit: 5, order: 'build_date DESC'});
+    });
+    return Q.all(promises);
+    }).then(function (builds) {
+    _.each(builds, function(build){
+      var buildHistory = _.map(build, function(dbBuild){
+        return dbBuild.dataValues.build_issuccess;
+      });
 
+      var pro = _.first(_.filter(projects, function(project){
+        return project.id === _.first(build).dataValues["ProjectId"];
+      }));
+      pro.lastBuilds = buildHistory;
+    });
+    res.json(projects);
+  });
 };
